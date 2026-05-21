54298d5e2905505703f6020edcd9c928da8e94e4
Aa993993 <1959087824@qq.com>
2026-05-21 20:49:40 +0800

    test: minimal hello world app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 android {
     namespace = "com.qqswitcher.demo"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.qqswitcher.demo"
         minSdk = 21
@@ -13,22 +12,11 @@ android {
         versionCode = 1
         versionName = "1.0.0"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.10.1")
 }
